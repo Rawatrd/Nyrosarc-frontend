9aa9fc6891cd8070c1f10311db3a2b7049277fe4
Dhanesh Rawat <rawatdhanesh990@gmail.com>
2025-05-10 14:53:24 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Nyros Ark</h1>
+      <p>Mystical knowledge, time patterns, and cognitive wonders await.</p>
+    </div>
+  );
+}
